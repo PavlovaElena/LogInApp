f7594bf8db8ca922d65b4ea81c0c9e86d0b55230
PavlovaElena <sferati@mail.ru>
2022-05-30 20:25:10 +0300

    Setup interface

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    private let userName = "User"
+    private let password = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    
+    @IBAction func loginButtonPressed() {
+    }
+    
+    @IBAction func showHelp(_ sender: UIButton) {
+    }
+    
+    
 }
 
